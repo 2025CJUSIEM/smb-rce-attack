b4c8a276022f89bef63a8d465f9c2e06ad40edf5
젬민 <tyler010333@gmail.com>
2025-06-01 19:10:06 +0900

    Create write.php

---
+++ board/write.php
@@ -0,0 +1,8 @@
+<h1>siem stealer SQL Injection</h1>
+<h2>write!</h2>
+<form method="post" action="insert.php">
+    제목: <input type="text" name="title"><br><br>
+    내용:<br>
+    <textarea name="content" rows="5" cols="50"></textarea><br><br>
+    <input type="submit" value="등록">
+</form>
